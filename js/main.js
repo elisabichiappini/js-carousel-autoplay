71f77c4bfc1dcabc49c38119a3b5606f8c3389cf
elisabichiappini <elisabichiappini@gmail.com>
2023-11-18 14:18:36 +0100

    feat: autoplay con bottoni js

--- js/main.js
+++ js/main.js
@@ -17,24 +17,37 @@ function next(){
     elementBoxesImagesDom[currentImage].classList.add('active');
 };
 
+function prev() { 
+    elementBoxesImagesDom[currentImage].classList.remove('active');
 
-
+    if (currentImage === 0) {
+        currentImage = elementBoxesImagesDom.length - 1; 
+    } else {
+        currentImage--;
+    }
+    elementBoxesImagesDom[currentImage].classList.add('active');
+}
 
 /*
 operations
 */
 
-//variabili
+// variabili
 const imagesLands = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
 
 const items = document.querySelector('.items');
 
 let currentImage = 0;
 
+// elementi in dom avanti e indietro
 const elementPrev = document.querySelector('.prev');
 const elementNext = document.querySelector('.next');
 
-//ciclo
+// eventi su avanti e indietro
+elementPrev.addEventListener('click', next);
+elementNext.addEventListener('click', prev);
+
+// ciclo e dom
 for(let i = 0; i < imagesLands.length; i++) {
 
     const elementBoxImage = document.createElement('div');
@@ -52,11 +65,27 @@ for(let i = 0; i < imagesLands.length; i++) {
     items.append(elementBoxImage);
 };
 
-// NodeList elementi 
+// NodeList elementi creati con ciclo
 const elementBoxesImagesDom = document.querySelectorAll('.item');
-console.log(elementBoxesImagesDom);
 
+// elementi in dom bottoni play e stop
+const playButton = document.querySelector('.play');
+const stopButton = document.querySelector('.stop');
 
-//eventi
-let autoplayCarousel = setInterval(next, 3000);
-autoplayCarousel;
+// Autoplay per bottoni
+let autoplayCarousel;
+
+// autoplay su bottoni play e stop
+playButton.addEventListener('click', next);
+stopButton.addEventListener('click', next);
+
+// eventi sui bottoni play e stop
+playButton.addEventListener('click', function() {
+    autoplayCarousel = setInterval(next, 3000);
+    console.log('ho cliccato play');
+});
+
+stopButton.addEventListener('click', function () {
+    clearInterval(autoplayCarousel);
+    console.log('ho cliccato stop');
+});
